ddb71a7fd8ee3b36abc6e746841747935d32754a
spoonsplz <modanhan@live.com>
2015-11-19 14:58:19 -0700

    initial commit

---
+++ src/l_3.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+// make sure to use good java naming convention
+
+public class l_3 {
+	public static void main(String[] args) {
+		Scanner HighQualityScanner = new Scanner(System.in);
+	}
+}
