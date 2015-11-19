79240ec2e3e3f3a7affd9a9ed9d30c4ec704399e
spoonsplz <modanhan@live.com>
2015-11-19 15:15:07 -0700

    copied codes from the website and deleted whatever doesnt compile, yeah im good at programming

--- src/l_3.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-// make sure to use good java naming convention
-
-public class l_3 {
-	public static void main(String[] args) {
-		Scanner HighQualityScanner = new Scanner(System.in);
-	}
-}
